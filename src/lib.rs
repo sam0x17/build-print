25e47903fa3948aecc65e72523f389e2fd475972
Sam Johnson <sam@durosoft.com>
2024-09-26 15:52:07 -0400

    add info!

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 #[doc(hidden)]
-pub mod _hidden {
+pub mod __hidden {
     pub use colored::*;
 }
 
@@ -10,7 +10,17 @@ macro_rules! println {
     };
     ($($arg:tt)*) => {
         ::std::println!("cargo:warning=\x1b[2K\r{}", ::std::format!($($arg)*));
-    };
+    }
+}
+
+#[macro_export]
+macro_rules! info {
+    ($($arg:tt)+) => {
+        {
+            use $crate::__hidden::*;
+            $crate::println!("{} {}", "info:".cyan().bold(), ::std::format!($($arg)+));
+        }
+    }
 }
 
 #[test]
@@ -19,4 +29,11 @@ fn test_println_compiles() {
     println!("hello world!");
     println!("hello {}", 33);
     println!("hello {}, {}, {}", 1, 2, 3);
+}
+
+#[test]
+fn test_info_compiles() {
+    info!("hello world!");
+    info!("hello {}", 33);
+    info!("hello {}, {}, {}", 1, 2, 3);
 }
