e256207c88871e8c11815899d3eef1a2689ad610
Sam Johnson <sam@durosoft.com>
2024-09-26 15:45:44 -0400

    fix println and handle all cases

--- src/lib.rs
+++ src/lib.rs
@@ -5,14 +5,18 @@ pub mod _hidden {
 
 #[macro_export]
 macro_rules! println {
+    () => {
+        ::std::println!("cargo:warning=\x1b[2K\r");
+    };
     ($($arg:tt)*) => {
-        {
-            ::std::println!("cargo:warning=\x1b[2K\r{}", $($arg)*)
-        }
-    }
+        ::std::println!("cargo:warning=\x1b[2K\r{}", ::std::format!($($arg)*));
+    };
 }
 
 #[test]
 fn test_println_compiles() {
+    println!();
     println!("hello world!");
+    println!("hello {}", 33);
+    println!("hello {}, {}, {}", 1, 2, 3);
 }
