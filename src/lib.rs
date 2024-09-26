0a674ee387c5ffd67a92a8d5867f2dd901fef119
Sam Johnson <sam@durosoft.com>
2024-09-26 15:28:01 -0400

    println support

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
+#[doc(hidden)]
+pub mod _hidden {
+    pub use colored::*;
+}
 
+#[macro_export]
+macro_rules! println {
+    ($($arg:tt)*) => {
+        {
+            ::std::println!("cargo:warning=\x1b[2K\r{}", $($arg)*)
+        }
+    }
+}
+
+#[test]
+fn test_println_compiles() {
+    println!("hello world!");
+}
