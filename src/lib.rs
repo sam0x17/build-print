d89d3b12357e0be7da7a4e5821c06357584a3ab0
Sam Johnson <sam@durosoft.com>
2024-09-26 18:36:10 -0400

    add lib.rs comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+//! A simple set of macros that allow regular printing as well as properly formatted info,
+//! warnings, errors, and notes during build scripts.
+//!
+//! Traditionally, there are only two ways to print to the console during a build script:
+//! * `println!("cargo:warning=...")` to print a somewhat annoyingly formatted warning message
+//! * `panic!(..)` to halt the build process and print an error message
+//!
+//! Regular `println!` statements are not shown during the build process, so this crate hijacks
+//! the `cargo:warning=...` variant using ANSI escape sequences to produce a working `println!`
+//! macro as well as `info!`, `warn!`, `error!`, and `note!` macros that following the
+//! indentation and coloring of standard cargo diagnostic messages.
+//!
+//! You can also define your own custom print messages using the `custom_println!` macro, which
+//! is also the basis for the other macros.
+
 #[macro_export]
 macro_rules! println {
     () => {
